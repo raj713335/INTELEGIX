9a74db3e09ae7381f73c332ad95b0f6c14ed5744
ARNAB DAS <raj713335@gmail.com>
2020-10-27 19:19:07 +0530

    Initial commit

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ def display():
         # OPTION SELECT POP UP CREATION
 
         def __init__(self, win):
+
             load = cv2.imread('Data/Images/Background/background1.png', 1)
             cv2imagex1 = cv2.cvtColor(load, cv2.COLOR_BGR2RGBA)
             load = Image.fromarray(cv2imagex1)
@@ -38,6 +39,8 @@ def display():
 
 
 
+
+
             self.b0 = tk.Button(win,
                                 bg='#33ff00',
                                 fg='#b7f731',
@@ -52,30 +55,30 @@ def display():
                                 width=20)
             self.b0r.place(x=1300, y=0, width=70, height=70)
 
-            self.b0b = tk.Button(win,
-                                 bg='#33ff00',
-                                 fg='#b7f731',
-                                 relief='flat',
-                                 width=20)
-            self.b0b.place(x=1300, y=700, width=70, height=70)
+            # self.b0b = tk.Button(win,
+            #                      bg='#33ff00',
+            #                      fg='#b7f731',
+            #                      relief='flat',
+            #                      width=20)
+            # self.b0b.place(x=1300, y=700, width=70, height=70)
 
-            self.b1 = ttk.Button(win, text='Class Enviroment', width=20)
-            self.b1.place(x=15, y=275, width=175, height=70)
+            self.b1 = ttk.Button(win, text='LIVE', width=20)
+            self.b1.place(x=385, y=225, width=250, height=70)
 
-            self.b2 = ttk.Button(win, text='Hostel Envioment', width=20)
-            self.b2.place(x=90, y=490, width=250, height=70)
+            self.b2 = ttk.Button(win, text='UPLOAD', width=20)
+            self.b2.place(x=385, y=325, width=250, height=70)
 
-            self.b3 = ttk.Button(win, text='Online Assignment', width=20)
-            self.b3.place(x=300, y=660, width=250, height=70)
+            self.b3 = ttk.Button(win, text='BROWSE', width=20)
+            self.b3.place(x=385, y=425, width=250, height=70)
 
-            self.b4 = ttk.Button(win, text='School Bus', width=20)
-            self.b4.place(x=1180, y=495, width=170, height=70)
-
-            self.b5 = ttk.Button(win, text='Corridor Enviroment', width=20)
-            self.b5.place(x=1050, y=380, width=200, height=70)
-
-            self.b6 = ttk.Button(win, text='START', width=20)
-            self.b6.place(x=1000, y=170, width=200, height=70)
+            # self.b4 = ttk.Button(win, text='School Bus', width=20)
+            # self.b4.place(x=1180, y=495, width=170, height=70)
+            #
+            # self.b5 = ttk.Button(win, text='Corridor Enviroment', width=20)
+            # self.b5.place(x=1050, y=380, width=200, height=70)
+            #
+            # self.b6 = ttk.Button(win, text='START', width=20)
+            # self.b6.place(x=1000, y=170, width=200, height=70)
 
             # button_over_ride = ttk.Button(win, height=1, width=1, bg='white', bd=0)
             # button_over_ride.place(x=0, y=1)
